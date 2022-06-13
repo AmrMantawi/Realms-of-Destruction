7204665e18e810cbd4fdf577c792654698baaca4
Amr El Mantawi <amr.mantawi@gmail.com>
2022-06-13 16:12:41 -0400

    movement setup

---
+++ Source/UnrealEngineGame/CharacterMovement.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CharacterMovement.h"
+
+// Sets default values
+ACharacterMovement::ACharacterMovement()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACharacterMovement::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACharacterMovement::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ACharacterMovement::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
