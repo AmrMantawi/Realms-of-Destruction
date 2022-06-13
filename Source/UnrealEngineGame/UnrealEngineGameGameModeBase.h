25a799e74832bf26ec08ef0ceeee19b98d50460b
Amr El Mantawi <amr.mantawi@gmail.com>
2022-06-13 11:21:25 -0400

    Created File

---
+++ Source/UnrealEngineGame/UnrealEngineGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealEngineGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALENGINEGAME_API AUnrealEngineGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
